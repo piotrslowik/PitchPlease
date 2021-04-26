25de7384148f9d71bdb3472ee72e917f41d9241d
piotrslowik <piotr.j.slowik@gmail.com>
2021-04-26 22:09:41 +0200

    Dodano widok dobrej i złej odpowiedzi

---
+++ app/src/main/java/com/example/pitchplease/AnswerCorrect.kt
@@ -0,0 +1,11 @@
+package com.example.pitchplease
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerCorrect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer_correct)
+    }
+}
