481d5ccc200d8de885cd14c98c63e350e18859f9
piotrslowik <piotr.j.slowik@gmail.com>
2021-04-27 22:20:27 +0200

    Nawigacja w menu głównym

--- app/src/main/java/com/example/pitchplease/MainActivity.kt
+++ app/src/main/java/com/example/pitchplease/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.pitchplease
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToDifficulty(view: View) {
+        val intent = Intent(this, Difficulty::class.java)
+        startActivity(intent)
+    }
+
+    fun goToSandbox(view: View) {
+        val intent = Intent(this, Sandbox::class.java)
+        startActivity(intent)
+    }
+
+    fun goToRanking(view: View) {
+        val intent = Intent(this, Ranking::class.java)
+        startActivity(intent)
+    }
+
+    fun goToPesonalization(view: View) {
+        val intent = Intent(this, Personalization::class.java)
+        startActivity(intent)
+    }
+
+    fun exitApp(view: View) {
+        finish()
     }
 }
